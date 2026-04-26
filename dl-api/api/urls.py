df1db20077cfc8ba1339cd6716a154cf6183ab6f
maripossa1 <maripossa1475@gmail.com>
2026-04-26 13:28:48 +0100

    Clear repository content

--- dl-api/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('predict/', views.PredictView.as_view(), name='predict'),
-    path('health/', views.HealthView.as_view(), name='health'),
-]
--- dl-api/dl_model/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DlModelConfig(AppConfig):
-    name = 'dl_model'
